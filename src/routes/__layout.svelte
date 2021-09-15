aa78e607bb3807243fc2ebf1fec5d4f5e8aeecf5
tempakyousuke <tempakyousuke@gmail.com>
2021-09-15 20:54:46 +0900

    create Footer component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 import Header from '$lib/header/Header.svelte';
+import Footer from '$lib/footer/Footer.svelte';
 import "../app.postcss";
 </script>
 <Header />
 <slot></slot>
+<Footer />
