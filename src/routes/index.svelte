e4d19f4ae9e428c7dc9ed8843251591ba4381941
tempakyousuke <tempakyousuke@gmail.com>
2021-09-15 20:49:05 +0900

    add section title and filter button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,8 +18,23 @@
 	<title>トップページ</title>
 </svelte:head>
 
-<div>
-	{#each posts as post}
-		<PostCard {post} />
-	{/each}
+<div class="px-6 py-8">
+	<div class="container flex justify-between mx-auto">
+		<div class="w-full lg:w-8/12">
+			<div class="flex items-center justify-between">
+				<h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
+				<div>
+					<select
+						class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+					>
+						<option>Latest</option>
+						<option>Last Week</option>
+					</select>
+				</div>
+			</div>
+			{#each posts as post}
+				<PostCard {post} />
+			{/each}
+		</div>
+	</div>
 </div>
