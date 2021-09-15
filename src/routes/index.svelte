17e763fcd47b4fcfa494d1c7f68183056d30f69e
tempakyousuke <tempakyousuke@gmail.com>
2021-09-15 21:44:32 +0900

    make sure the left edges are aligned

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,25 +22,27 @@
 <div class="px-6 py-8">
 	<div class="container flex justify-between mx-auto">
 		<div class="w-full lg:w-8/12">
-			<div class="flex items-center justify-between">
-				<h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
-				<div>
-					<select
-						class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-					>
-						<option>Latest</option>
-						<option>Last Week</option>
-					</select>
+			<div class="max-w-4xl mx-auto">
+				<div class="flex items-center justify-between">
+					<h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
+					<div>
+						<select
+							class="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+						>
+							<option>Latest</option>
+							<option>Last Week</option>
+						</select>
+					</div>
 				</div>
-			</div>
-			{#each posts as post}
-				<div class="mt-6">
-					<PostCard {post} />
-				</div>
-			{/each}
+				{#each posts as post}
+					<div class="mt-6">
+						<PostCard {post} />
+					</div>
+				{/each}
 
-			<div class="mt-8 max-w-4xl mx-auto">
-				<Pagination />
+				<div class="mt-8">
+					<Pagination />
+				</div>
 			</div>
 		</div>
 	</div>
