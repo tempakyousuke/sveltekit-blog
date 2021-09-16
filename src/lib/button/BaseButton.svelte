60cdbe73f7e997f14171416e508403b31b32ea95
tempakyousuke <tempakyousuke@gmail.com>
2021-09-17 03:55:55 +0900

    create button component

---
+++ src/lib/button/BaseButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	export let block = false;
+	export let disabled = false;
+	export let buttonClass = '';
+	export let className = '';
+
+	const dispatch = createEventDispatcher();
+	const handleClick = () => {
+		if (!disabled) {
+			dispatch('click');
+		}
+	};
+</script>
+
+<div class="inline-block {className}" class:w-full={block}>
+	<button
+		class="button {buttonClass}"
+		class:w-full={block}
+		class:disabled
+		type="button"
+		on:click={handleClick}
+	>
+		<slot />
+	</button>
+</div>
+
+<style type="text/postcss">
+	.button {
+		@apply py-1 px-3 rounded focus:outline-none bg-opacity-100;
+	}
+	.disabled {
+		opacity: 0.7;
+	}
+</style>
