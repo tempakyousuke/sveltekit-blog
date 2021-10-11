c7dea446ee676bfd9e88ed7e9b69dc755183df28
tempakyousuke <tempakyousuke@gmail.com>
2021-10-11 18:26:37 +0900

    validate signin and signup form

--- src/routes/signin.svelte
+++ src/routes/signin.svelte
@@ -1,15 +1,60 @@
 <script lang="ts">
+	import * as yup from 'yup';
 	import Input from '$lib/forms/Input.svelte';
 	import Button from '$lib/button/Button.svelte';
 	import { auth } from '$modules/firebase/firebase';
 	import { signInWithEmailAndPassword } from 'firebase/auth';
 	import { goto } from '$app/navigation';
 
-	let email = '';
-	let password = '';
+	let values = {
+		email: '',
+		password: ''
+	};
+
+	let errors = {
+		email: '',
+		password: ''
+	};
+
+	$: hasError = errors.email !== '' || errors.password !== '';
+
+	const schema = yup.object().shape({
+		email: yup
+			.string()
+			.required('メールアドレスは必須です')
+			.email('メールアドレスが正しくありません'),
+		password: yup
+			.string()
+			.required('パスワードは必須です')
+			.min(6, 'パスワードは6文字以上入力してください')
+	});
+
+	const validate = (path: string) => {
+		schema
+			.validateAt(path, values)
+			.then(() => {
+				errors[path] = '';
+			})
+			.catch((err) => {
+				errors[path] = err.message;
+			});
+	};
+
+	const submit = () => {
+		schema
+			.validate(values, { abortEarly: false })
+			.then(() => {
+				signIn();
+			})
+			.catch((err) => {
+				err.inner.forEach((error) => {
+					errors[error.path] = error.message;
+				});
+			});
+	};
 
 	const signIn = () => {
-		signInWithEmailAndPassword(auth, email, password)
+		signInWithEmailAndPassword(auth, values.email, values.password)
 			.then(() => {
 				goto('/admin');
 			})
@@ -23,9 +68,21 @@
 	<div class="flex flex-col px-4 py-6 bg-white rounded-lg shadow-md w-full max-w-md">
 		<h1>ログイン</h1>
 		<form>
-			<Input bind:value={email} type="email" label="メールアドレス" className="mt-2" />
-			<Input bind:value={password} type="password" label="パスワード" className="mt-2" />
-			<Button block className="mt-5" on:click={signIn}>ログイン</Button>
+			<Input
+				bind:value={values.email}
+				type="email"
+				label="メールアドレス"
+				className="mt-2"
+				on:input={() => validate('email')}
+			/>
+			<Input
+				bind:value={values.password}
+				type="password"
+				label="パスワード"
+				className="mt-2"
+				on:input={() => validate('password')}
+			/>
+			<Button block className="mt-5" on:click={submit} disabled={hasError}>ログイン</Button>
 		</form>
 	</div>
 </div>
--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import * as yup from 'yup';
 	import Input from '$lib/forms/Input.svelte';
 	import Button from '$lib/button/Button.svelte';
 	import { auth, db } from '$modules/firebase/firebase';
@@ -6,16 +7,62 @@
 	import { doc, setDoc } from 'firebase/firestore';
 	import { goto } from '$app/navigation';
 
-	let email = '';
-	let password = '';
-	let name = '';
+	let values = {
+		name: '',
+		email: '',
+		password: ''
+	};
+
+	let errors = {
+		name: '',
+		email: '',
+		password: ''
+	};
+
+	$: hasError = errors.name !== '' || errors.email !== '' || errors.password !== '';
+
+	const schema = yup.object().shape({
+		name: yup.string().required('名前は必須です'),
+		email: yup
+			.string()
+			.required('メールアドレスは必須です')
+			.email('メールアドレスが正しくありません'),
+		password: yup
+			.string()
+			.required('パスワードは必須です')
+			.min(6, 'パスワードは6文字以上入力してください')
+	});
+
+	const validate = (path: string) => {
+		schema
+			.validateAt(path, values)
+			.then(() => {
+				errors[path] = '';
+			})
+			.catch((err) => {
+				errors[path] = err.message;
+			});
+	};
+
+	const submit = () => {
+		schema
+			.validate(values, { abortEarly: false })
+			.then(() => {
+				signUp();
+			})
+			.catch((err) => {
+				err.inner.forEach((error) => {
+					errors[error.path] = error.message;
+				});
+			});
+	};
 
 	const signUp = () => {
-		createUserWithEmailAndPassword(auth, email, password)
+		createUserWithEmailAndPassword(auth, values.email, values.password)
 			.then(async (userCredential) => {
 				const user = userCredential.user;
 				await setDoc(doc(db, 'users', user.uid), {
-					name,
+					name: values.name,
 					allowed: false
 				});
 				goto('/');
@@ -30,10 +77,28 @@
 	<div class="flex flex-col px-4 py-6 bg-white rounded-lg shadow-md w-full max-w-md">
 		<h1>新規登録</h1>
 		<form>
-			<Input bind:value={name} type="email" label="名前" className="mt-2" />
-			<Input bind:value={email} type="email" label="メールアドレス" className="mt-2" />
-			<Input bind:value={password} type="password" label="パスワード" className="mt-2" />
-			<Button block className="mt-5" on:click={signUp}>登録</Button>
+			<Input
+				bind:value={values.name}
+				type="email"
+				label="名前"
+				className="mt-2"
+				on:input={() => validate('name')}
+			/>
+			<Input
+				bind:value={values.email}
+				type="email"
+				label="メールアドレス"
+				className="mt-2"
+				on:input={() => validate('email')}
+			/>
+			<Input
+				bind:value={values.password}
+				type="password"
+				label="パスワード"
+				className="mt-2"
+				on:input={() => validate('password')}
+			/>
+			<Button block className="mt-5" on:click={submit}>登録</Button>
 		</form>
 	</div>
 </div>
