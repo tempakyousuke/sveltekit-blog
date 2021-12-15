e5e421d000d2dbbf4b45463af294a6f498b71301
tempakyousuke <tempakyousuke@gmail.com>
2021-12-15 19:18:57 +0900

    prepare title input

--- src/routes/admin/post/create.svelte
+++ src/routes/admin/post/create.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import Input from '$lib/forms/Input.svelte';
+	let values = {
+		title: '',
+		body: ''
+	};
+
+	let errors = {
+		title: '',
+		body: ''
+	};
 </script>
 
 <svelte:head>
 	<title>新規記事作成</title>
 </svelte:head>
+
+<div class="container">
+	<Input bind:value={values.title} />
+</div>
