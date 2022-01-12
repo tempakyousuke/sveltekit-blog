a281d086e4588e4b0154ab895afd7dc8b68626fa
tempakyousuke <tempakyousuke@gmail.com>
2022-01-13 04:35:06 +0900

    prepare author page

---
+++ src/routes/author/[identifier].svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	export async function load({ page }) {
+		const identifier = page.params.identifier;
+		const q = query(
+			collection(db, 'users'),
+			where('identifier', '==', identifier),
+			orderBy('modified', 'desc')
+		);
+		const authors = await UserModelFactory.getList(q);
+		const author = authors[0];
+		const qp = query(collection(db, 'posts'), where('uid', '==', author.id));
+		const posts = await PostModelFactory.getList(qp);
+		return {
+			props: {
+				author,
+				posts
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import { PostModelFactory } from '$model/post';
+	import { UserModelFactory } from '$model/user';
+	import type { PostModel } from '$model/post';
+	import type { UserModel } from '$model/user';
+	import { query, where, collection, orderBy } from 'firebase/firestore';
+	import { db } from '$modules/firebase/firebase';
+	import AuthorCard from '$lib/author/AuthorCard.svelte';
+
+	export let posts: PostModel[];
+	export let author: UserModel;
+</script>
+
+<div class="pt-16">
+	<AuthorCard {author} className="max-w-lg mx-auto" />
+</div>
