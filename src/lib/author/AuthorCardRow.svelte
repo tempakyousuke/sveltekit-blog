dd285113eb7e7541ad4cda49525b20287d041674
tempakyousuke <tempakyousuke@gmail.com>
2022-01-13 14:58:12 +0900

    create AuthorCardRow

---
+++ src/lib/author/AuthorCardRow.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { UserModel } from '$model/user';
+	export let author: UserModel;
+	export let className = '';
+</script>
+
+<div class="flex px-6 py-4 bg-white rounded-lg shadow-md {className}">
+	<a href="/author/{author.identifier}">
+		<img
+			src={author.imageUrl}
+			alt={author.name}
+			class="object-cover w-32 h-32 rounded-full mx-auto"
+		/>
+	</a>
+	<div class="flex-grow ml-10">
+		<div class="mt-4 text-xl">
+			<a href="/author/{author.identifier}">
+				{author.name}
+			</a>
+		</div>
+		<div class="mt-4 whitespace-pre-wrap text-sm">{author.introduction}</div>
+	</div>
+</div>
--- src/routes/post/[id].svelte
+++ src/routes/post/[id].svelte
@@ -2,9 +2,11 @@
 	export async function load({ page }) {
 		const id = page.params.id;
 		const post = await PostModelFactory.getDoc(id);
+		const author = await UserModelFactory.getDoc(post.uid);
 		return {
 			props: {
-				post
+				post,
+				author
 			}
 		};
 	}
@@ -12,57 +14,38 @@
 
 <script lang="ts">
 	import { PostModelFactory } from '$model/post';
+	import { UserModelFactory } from '$model/user';
+	import AuthorCardRow from '$lib/author/AuthorCardRow.svelte';
+	import type { PostModel } from '$model/post';
+	import type { UserModel } from '$model/user';
 
-	export let post;
-	const author = {
-		id: 'aaaa',
-		name: 'tempakyousuke',
-		image: '/tmp_profile.jpeg',
-		postCount: '100'
-	};
+	export let post: PostModel;
+	export let author: UserModel;
 </script>
 
 <svelte:head>
 	<title>{post.title}</title>
 </svelte:head>
-<div class="px-10 py-6 bg-white rounded-lg shadow-md mx-auto w-10/12">
-	<div class="flex items-center justify-between">
-		<span class="font-light text-gray-600">{post.createdDatetime}</span>
-		{#each post.tags as tag}
-			<a
-				sveltekit:prefetch
-				href={`/tag/${tag}`}
-				class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">{tag}</a
-			>
-		{/each}
-	</div>
-	<div class="mt-2">
-		<div class="text-2xl font-bold text-gray-700 hover:underline">
-			{post.title}
+<div class="mx-auto w-10/12">
+	<div class="px-10 py-6 bg-white rounded-lg shadow-md">
+		<div class="flex items-center justify-between">
+			<span class="font-light text-gray-600">{post.createdDatetime}</span>
+			{#each post.tags as tag}
+				<a
+					sveltekit:prefetch
+					href={`/tag/${tag}`}
+					class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">{tag}</a
+				>
+			{/each}
 		</div>
-		<p class="mt-2 text-gray-600">
-			{@html post.htmlBody}
-		</p>
-	</div>
-	<div class="flex items-center justify-between mt-4">
-		<div>
-			<a href="/author/{author.id}" class="flex items-center"
-				><img
-					src={author.image}
-					alt="avatar"
-					class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block"
-				/>
-				<h1 class="font-bold text-gray-700 hover:underline">{author.name}</h1>
-			</a>
+		<div class="mt-2">
+			<div class="text-2xl font-bold text-gray-700 hover:underline">
+				{post.title}
+			</div>
+			<p class="mt-2 text-gray-600">
+				{@html post.htmlBody}
+			</p>
 		</div>
 	</div>
+	<AuthorCardRow {author} className="mt-10" />
 </div>
-
-<style type="text/postcss">
-	.tab {
-		@apply border border-gray-300 cursor-pointer py-2 px-4 rounded-t-md;
-	}
-	.active {
-		@apply border-b-0 text-blue-500;
-	}
-</style>
