f43155d426fe8fda92e5d0b1fcff9064433aa224
tempakyousuke <tempakyousuke@gmail.com>
2024-01-15 03:48:01 +0900

    show authors only having one more post

--- src/lib/author/AuthorListCard.svelte
+++ src/lib/author/AuthorListCard.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import type { UserModel } from '$model/user';
 	export let authors!: UserModel[];
+	$: filteredAuthors = authors.filter((author) => author.postCount > 0);
 </script>
 
 <div class="flex flex-col px-6 py-4 bg-white rounded-lg shadow-md">
 	<ul class="-mx-4 -pt-4">
-		{#each authors as author, i}
+		{#each filteredAuthors as author, i}
 			<li class="flex items-center {i !== 0 ? 'mt-6' : ''}">
 				<img src={author.imageUrl} alt="avatar" class="object-cover w-10 h-10 mx-4 rounded-full" />
 				<p>
