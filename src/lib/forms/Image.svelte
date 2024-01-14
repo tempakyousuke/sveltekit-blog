6f7e4df6a5ae94a5679ab8d8384191f73a807753
tempakyousuke <tempakyousuke@gmail.com>
2024-01-14 18:24:44 +0900

    refactoring codes

--- src/lib/forms/Image.svelte
+++ src/lib/forms/Image.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	export let defaultImage = '';
 	export let file;
-	let files;
-	let input;
+	let files: FileList;
+	let input: HTMLInputElement;
 	let pickedImage = '';
 
 	const selectFile = () => {
 		input.click();
 	};
 
-	const dropFile = (event) => {
-		files = event.dataTransfer.files;
+	const dropFile = (event: DragEvent) => {
+		if (event.dataTransfer) {
+			files = event.dataTransfer.files;
+		}
 	};
 	$: {
 		if (files !== undefined) {
@@ -34,6 +36,8 @@
 </script>
 
 <div
+	role="button"
+	tabindex="0"
 	class="fles border h-48 bg-white items-center justify-center"
 	on:drop|preventDefault={(event) => dropFile(event)}
 	on:dragover|preventDefault
@@ -41,12 +45,28 @@
 	{#if previewImage === ''}
 		<div>
 			ここに画像をドロップするか
-			<div class="text-blue-500 hover:text-blue-300 cursor-pointer" on:click={selectFile}>
+			<div
+				role="button"
+				tabindex="0"
+				class="text-blue-500 hover:text-blue-300 cursor-pointer"
+				on:click={selectFile}
+				on:keydown={(event) => {
+					if (event.key === 'Enter') selectFile();
+				}}
+			>
 				ファイルを選択
 			</div>
 		</div>
 	{:else}
-		<img src={previewImage} alt="プレビュー" class="h-full mx-auto" on:click={selectFile} />
+		<button
+			class="h-full mx-auto block"
+			on:click={selectFile}
+			on:keydown={(event) => {
+				if (event.key === 'Enter') selectFile();
+			}}
+		>
+			<img src={previewImage} alt="プレビュー" class="h-full mx-auto" />
+		</button>
 	{/if}
 	<input class="hidden" type="file" accept="image/*" bind:this={input} bind:files />
 </div>
