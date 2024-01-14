3cf6f866817eccc49a9192655bd46bf2814960c1
tempakyousuke <tempakyousuke@gmail.com>
2024-01-15 01:51:00 +0900

    Refactor user authentication logic and fix header link

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -7,13 +7,15 @@
 	onAuthStateChanged(auth, async (firebaseUser) => {
 		if (firebaseUser) {
 			const userDoc = await getDoc(doc(db, 'users', firebaseUser.uid));
-			const userData = userDoc.data();
-			user.set({
-				uid: firebaseUser.uid,
-				name: userData.name,
-				allowed: userData.allowed,
-				isLoggedIn: true
-			});
+			if (userDoc.exists()) {
+				const userData = userDoc.data();
+				user.set({
+					uid: firebaseUser.uid,
+					name: userData.name,
+					allowed: userData.allowed,
+					isLoggedIn: true
+				});
+			}
 		} else {
 			user.set({
 				uid: '',
@@ -36,9 +38,7 @@
 		<div class="container flex flex-col mx-auto md:flex-row md:items-center md:justify-between">
 			<div class="flex items-center justify-between">
 				<div>
-					<a sveltekit:prefetch href="/" class="text-xl font-bold text-gray-800 md:text-2xl"
-						>Azumaya Blog</a
-					>
+					<a href="/" class="text-xl font-bold text-gray-800 md:text-2xl">Azumaya Blog</a>
 				</div>
 				<!-- <div>
 					<button
