a4e323bd85238964c13a7057a6402148d555ba22
tempakyousuke <tempakyousuke@gmail.com>
2024-01-15 03:45:49 +0900

    fix order of top page posts

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -4,14 +4,12 @@ import { query, where, collection } from 'firebase/firestore';
 import { db } from '$modules/firebase/firebase';
 
 export async function load() {
-  const q = query(collection(db, 'posts'), where('status', '==', 'public'));
   const qu = query(collection(db, 'users'), where('allowed', '==', true));
 
   const authors = await UserModelFactory.getList(qu);
-  const posts = await PostModelFactory.getList(q);
+  const posts = await PostModelFactory.getList();
   return {
       posts,
       authors
-
   };
 }
