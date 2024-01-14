cc4dc4e5c0f6bc5ef24b60cf79db32d5feff5891
tempakyousuke <tempakyousuke@gmail.com>
2024-01-14 21:15:59 +0900

    Refactor AdminHeader.svelte to use button element for logout

--- src/lib/header/AdminHeader.svelte
+++ src/lib/header/AdminHeader.svelte
@@ -11,28 +11,14 @@
 		<div class="container flex flex-col mx-auto md:flex-row md:items-center md:justify-between">
 			<div class="flex items-center justify-between">
 				<div>
-					<a sveltekit:prefetch href="/" class="text-xl font-bold text-gray-800 md:text-2xl">
-						Azumaya Blog
-					</a>
-				</div>
-				<div>
-					<button
-						type="button"
-						class="block text-gray-800 hover:text-gray-600 focus:text-gray-600 focus:outline-none md:hidden"
-					>
-						<svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-							<path
-								d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-							/>
-						</svg>
-					</button>
+					<a href="/" class="text-xl font-bold text-gray-800 md:text-2xl"> Azumaya Blog </a>
 				</div>
 			</div>
 			<div class="flex-col hidden md:flex md:flex-row md:-mx-4">
 				<a class="nav-link" href="/admin/">管理画面トップ</a>
 				<a class="nav-link" href="/admin/post/create">新規記事</a>
 				<a class="nav-link" href="/admin/profile">プロフィール</a>
-				<span class="nav-link" on:click={logout}>ログアウト</span>
+				<button class="nav-link" on:click={logout}>ログアウト</button>
 			</div>
 		</div>
 	</nav>
